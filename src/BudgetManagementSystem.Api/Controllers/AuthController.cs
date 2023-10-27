9690df942bcd709a6f2ecad85b8c19c2f11c6c22
Simona Ragauskaite <simonaaragauskaite@gmail.com>
2023-10-27 21:14:57 +0300

    minot fix

--- src/BudgetManagementSystem.Api/Controllers/AuthController.cs
+++ src/BudgetManagementSystem.Api/Controllers/AuthController.cs
@@ -123,7 +123,7 @@ namespace BudgetManagementSystem.Api.Controllers
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
                 _config["Jwt:Audience"],
                 claims,
-                expires: DateTime.Now.AddMinutes(15),
+                expires: DateTime.Now.AddHours(2),
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
